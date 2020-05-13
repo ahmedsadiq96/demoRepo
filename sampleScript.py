e6f46d23118c0f5570408d7fe8f0ea8aee750896
Ahmed Sadiq <ahmedsadiqu@hotmail.com>
2020-05-13 11:48:34 -0600

    changed second matrix

--- sampleScript.py
+++ sampleScript.py
@@ -7,3 +7,12 @@ Created on Wed May 13 11:33:29 2020
 """
 
 
+import numpy as np
+import pandas as pd
+
+my_matrix =np.eye(4)
+my_second_matrix=np.array[[1,2,3,4],
+                          [2,3,4]]
+
+print(my_matrix)
+print(my_second_matrix)
